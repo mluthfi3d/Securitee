68bb55db2200991404d22f2712bea46c1853c734
mluthfi3d <mluthfi3d@gmail.com>
2023-09-08 23:00:49 +0700

    adding SwiftKeychainWrapper

--- Securitee/ContentView.swift
+++ Securitee/ContentView.swift
@@ -6,17 +6,43 @@
 //
 
 import SwiftUI
+import SwiftKeychainWrapper
 
 struct ContentView: View {
+    @State var contentToShow = "empty"
+    @State var contentToHide = ""
+    @State var label = "Your text would be shown below here"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (spacing: 16) {
+            VStack (spacing: 8){
+                Text(label)
+                    .font(.system(size: 12))
+                Text(contentToShow)
+                    .font(.system(size: 18))
+            }
+            TextField("Content to Hide", text: $contentToHide)
+                .font(.system(size: 18))
+                .lineLimit(4)
+                .textFieldStyle(.roundedBorder)
+            Button{
+                KeychainWrapper.standard.set(contentToHide, forKey: "contentToHide")
+                label = "Now close your app and open it again"
+            } label : {
+                Text("Hide")
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
+        .padding([.horizontal], 16)
+        
+        .onAppear(){
+            contentToShow = KeychainWrapper.standard.string(forKey: "contentToHide") ?? "You didn't save any content"
+            if contentToShow != "You didn't save any content" {
+                label = "This demo provide information about SwiftKeychainWrapper that is more secure than UserDefaults to save sensitive data"
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
