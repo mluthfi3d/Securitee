8a4439386c8153537ee5a72791ff602950696519
mluthfi3d <mluthfi3d@gmail.com>
2023-09-08 22:43:53 +0700

    Initial Commit

---
+++ Securitee/SecuriteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecuriteeApp.swift
+//  Securitee
+//
+//  Created by Muhammad Luthfi on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SecuriteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
